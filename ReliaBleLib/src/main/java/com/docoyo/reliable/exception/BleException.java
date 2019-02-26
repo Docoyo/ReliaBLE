02a18c56176855433f53778b57bc9752b9f5626b
Nicolaj Kirchhof <nico@docoyo.com>
2019-02-27 00:05:36 +0100

    Made constants public again

--- ReliaBleLib/src/main/java/com/docoyo/reliable/exception/BleException.java
+++ ReliaBleLib/src/main/java/com/docoyo/reliable/exception/BleException.java
@@ -7,9 +7,9 @@ public abstract class BleException implements Serializable {
 
     private static final long serialVersionUID = 8004414918500865564L;
 
-    static final int ERROR_CODE_TIMEOUT = 100;
-    static final int ERROR_CODE_GATT = 101;
-    static final int ERROR_CODE_OTHER = 102;
+    public static final int ERROR_CODE_TIMEOUT = 100;
+    public static final int ERROR_CODE_GATT = 101;
+    public static final int ERROR_CODE_OTHER = 102;
 
     private int code;
     private String description;
